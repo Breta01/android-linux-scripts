10ba89cab55258f1d9b959ea96ace971e350d2c2
Břetislav Hájek <breta.hajek@gmail.com>
2017-02-27 19:08:00 +0100

    Remove unnecessary package

--- android_jupyter_install.sh
+++ android_jupyter_install.sh
@@ -2,13 +2,12 @@
 # Update
 apt-get update && apt-get upgrade -y
 # Install python+packages and curl
-apt-get install -y python3 python3-pip python3-numpy python3-scipy python3-matplotlib ipython3 ipython3-notebook python3-pandas python3-nose curl wget
+apt-get install -y python3 python3-pip python3-numpy python3-scipy python3-matplotlib ipython3 ipython3-notebook python3-pandas python3-nose curl
 # Update pip
 python3 -m easy_install -U pip
 
 # Correctinng python file 
 # which causes stuck during package installation
-cd /
 curl https://gist.githubusercontent.com/Breta01/acdf05e37efcebdab05dbd95ce14394a/raw/linklockfile.py > /usr/local/lib/python3.4/dist-packages/pip-9.0.1-py3.4.egg/pip/_vendor/lockfile/linklockfile.py
 
 # Get requirements file
