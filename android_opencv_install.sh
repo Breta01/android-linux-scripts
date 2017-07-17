60e90c75da79f74fdc0d788c6f5efe1a2f536a97
Breta01 <breta.hajek@gmail.com>
2017-07-17 20:28:26 +0000

    Typo and location fix

--- android_opencv_install.sh
+++ android_opencv_install.sh
@@ -6,7 +6,7 @@
 apt-get update && apt-get upgrade -y
 
 # Install dependencies
-apt-get -y install libopencv-dev curl build-essential cmake libgtk2.0-dev pkg-config python3-dev python3-numpy python-dev python-numpy libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utilsi
+apt-get -y install libopencv-dev curl build-essential cmake libgtk2.0-dev pkg-config python3-dev python3-numpy python-dev python-numpy libdc1394-22-dev libjpeg-dev libpng12-dev libtiff5-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils
 
 # Getting latest stable version:
 # Right now 3.2.0
@@ -18,7 +18,7 @@ mkdir opencv-build && cd opencv-build
 
 # Configurate make
 # I get some errors with 1394 and ffmpeg (needs additional libraries)
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_1394=OFF -D WITH_FFMPEG=OFF ../opencv-3.2.0
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_1394=OFF -D WITH_FFMPEG=OFF /tmp/opencv-3.2.0
 
 # Make and install
 # This may take a while
